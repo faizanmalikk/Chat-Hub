0ab9da7449de3ccca287c97d080b474758336eee
faizanmalikk <faizanjamil656@gmail.com>
2023-07-18 02:07:14 +0500

    bugs

--- app/api/pusher/auth/route.ts
+++ app/api/pusher/auth/route.ts
@@ -1,27 +1,29 @@
-import { NextApiRequest, NextApiResponse } from "next"
-import { getServerSession } from "next-auth";
+// import { NextApiRequest, NextApiResponse } from "next"
+// import { getServerSession } from "next-auth";
 
-import { pusherServer } from "@/app/utils/pusher";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { NextRequest, NextResponse } from "next/server";
+
+// import { pusherServer } from "@/app/utils/pusher";
+// import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 const handler = async (
-    request: NextApiRequest,
-    response: NextApiResponse
+    request: NextRequest,
+    response : NextResponse
 ) => {
-    const session = await getServerSession(request, response, authOptions);
+    // const session = await getServerSession(request, response, authOptions);
 
-    if (!session?.user?.email) {
-        return response.status(401);
-    }
+    // if (!session?.user?.email) {
+    //     return response.status(401);
+    // }
 
-    const socketId = request.body.socket_id;
-    const channel = request.body.channel_name;
-    const data = {
-        user_id: session.user.email,
-    };
+    // const socketId = request.body.socket_id;
+    // const channel = request.body.channel_name;
+    // const data = {
+    //     user_id: session.user.email,
+    // };
 
-    const authResponse = pusherServer.authorizeChannel(socketId, channel, data);
-    return response.send(authResponse);
+    // const authResponse = pusherServer.authorizeChannel(socketId, channel, data);
+    // return response.send(authResponse);
 };
 
 export { handler as GET, handler as POST };
